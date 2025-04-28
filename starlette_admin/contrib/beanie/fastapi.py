c555fb4950188b23be6cc98c4a3f26a3e25bc02e
Alexander Lukens <alexdlukens@gmail.com>
2025-04-27 21:40:11 -0400

    run hatch format

--- starlette_admin/contrib/beanie/fastapi.py
+++ starlette_admin/contrib/beanie/fastapi.py
@@ -1,8 +1,9 @@
+from typing import List, Optional
+
 from fastapi import APIRouter, Query
+from starlette.requests import Request
 from starlette_admin.contrib.beanie import ModelView
 from starlette_admin.contrib.beanie.converters import BeanieModelConverter
-from typing import Optional, List
-from starlette.requests import Request
 
 
 class FastAPIModelView(ModelView):
@@ -17,13 +18,35 @@ class FastAPIModelView(ModelView):
         prefix: str = "",
     ):
         super().__init__(document, icon, name, label, identity, converter)
-        self.router = APIRouter(prefix=f"/{prefix}{self.identity}", tags=[self.identity])
+        self.router = APIRouter(
+            prefix=f"/{prefix}{self.identity}", tags=[self.identity]
+        )
 
-        self.router.add_api_route("/edit", self.edit, methods=["PATCH"], response_model=self.document)
-        self.router.add_api_route("/create", self.create, methods=["POST"], response_model=self.document)
-        self.router.add_api_route("/delete", self.delete, methods=["DELETE"], response_model=Optional[int])
-        self.router.add_api_route("/get", self.find_by_pk, methods=["GET"], response_model=Optional[self.document])
-        self.router.add_api_route("/list", self.find_all, methods=["GET"], response_model=list[self.document])
+        self.router.add_api_route(
+            "/edit", self.edit, methods=["PATCH"], response_model=self.document
+        )
+        self.router.add_api_route(
+            "/create", self.create, methods=["POST"], response_model=self.document
+        )
+        self.router.add_api_route(
+            "/delete", self.delete, methods=["DELETE"], response_model=Optional[int]
+        )
+        self.router.add_api_route(
+            "/get",
+            self.find_by_pk,
+            methods=["GET"],
+            response_model=Optional[self.document],
+        )
+        self.router.add_api_route(
+            "/list", self.find_all, methods=["GET"], response_model=list[self.document]
+        )
 
-    async def find_all(self, request: Request, skip: int = Query(default=0), limit: int = Query(default=100), where: str = Query(""), order_by: List[str] = Query(["id asc"])):
+    async def find_all(
+        self,
+        request: Request,
+        skip: int = Query(default=0),
+        limit: int = Query(default=100),
+        where: str = Query(""),
+        order_by: List[str] = Query(["id asc"]),
+    ):
         return await super().find_all(request, skip, limit, where, order_by)
